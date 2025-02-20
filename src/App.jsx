764fbf2ac42e82fae8374aeaeaf3651107bed5ca
Fereshteakbari <fereshteakbariam18@gmail.com>
2025-02-20 17:58:21 +0330

    Fix Git ownership issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header.jsx";
-import Contacts from "./components/contacts.jsx";
+import Contacts from "./components/Contacts.jsx";
 function App() {
   return (
     <>
